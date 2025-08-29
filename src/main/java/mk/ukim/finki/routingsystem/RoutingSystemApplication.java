95819567675e97dafe67cc10c192780f1057b8fb
Jovana <jovana.jovanoska003@gmail.com>
2025-08-29 22:10:08 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/routingsystem/RoutingSystemApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.routingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingSystemApplication.class, args);
+	}
+
+}
