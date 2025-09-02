4bdff67be6df3ce649004f9feaa636bf6702afab
Jovana <jovana.jovanoska003@gmail.com>
2025-09-02 19:07:05 +0200

    feat(user): update Employee entity

--- src/main/java/mk/ukim/finki/routingsystem/model/Employee.java
+++ src/main/java/mk/ukim/finki/routingsystem/model/Employee.java
@@ -5,40 +5,47 @@ import lombok.Data;
 import mk.ukim.finki.routingsystem.model.enumerations.EmployeeType;
 import mk.ukim.finki.routingsystem.model.enumerations.Role;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 public class Employee {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "seq_gen")
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
-    private String password;
+    @Column(nullable = false, length = 100)
+    private String passwordHash;
 
     private String firstName;
 
     private String lastName;
 
     @ManyToOne (fetch = FetchType.LAZY)
-    @JoinColumn(name="department_id")
     private Department department;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
     @Enumerated (EnumType.STRING)
+    @Column(nullable = false)
     private EmployeeType type;
+
+    @ManyToMany(mappedBy = "routedToEmployees")
+    private List <Document> documents = new ArrayList<>();
 
     public Employee() {
     }
 
-    public Employee(String email, String password, String firstName, String lastName, Department department, Role role, EmployeeType type) {
+    public Employee(String email, String passwordHash, String firstName, String lastName, Department department, Role role, EmployeeType type) {
         this.email = email;
-        this.password = password;
+        this.passwordHash = passwordHash;
         this.firstName = firstName;
         this.lastName = lastName;
         this.department = department;
