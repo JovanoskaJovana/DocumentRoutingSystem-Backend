35bd682980024458c9e5a98c0c15b9b5d9257e9f
Jovana <jovana.jovanoska003@gmail.com>
2025-09-15 09:14:02 +0200

    refactor(employee): adjust Employee and EmployeeDto for service layer

--- src/main/java/mk/ukim/finki/routingsystem/model/dto/EmployeeDto.java
+++ src/main/java/mk/ukim/finki/routingsystem/model/dto/EmployeeDto.java
@@ -1,13 +1,19 @@
 package mk.ukim.finki.routingsystem.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import mk.ukim.finki.routingsystem.model.enumerations.EmployeeType;
+import mk.ukim.finki.routingsystem.model.enumerations.Role;
+
 public record EmployeeDto(
 
         Long employeeId,
         String email,
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        String password,
         String firstName,
         String lastName,
-        String department,
-        String role,
-        String employeeType
+        Long departmentId,
+        Role role,
+        EmployeeType employeeType
 
 ) {}
