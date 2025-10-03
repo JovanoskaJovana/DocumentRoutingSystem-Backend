4a60d821fef130ccc3181f33fe92f376a5f3ba18
Jovana <jovana.jovanoska003@gmail.com>
2025-10-03 15:37:36 +0200

    feat(controller): add DocumentActionRestController with endpoints for managing document actions

---
+++ src/main/java/mk/ukim/finki/routingsystem/web/DocumentActionRestController.java
@@ -0,0 +1,36 @@
+package mk.ukim.finki.routingsystem.web;
+
+import mk.ukim.finki.routingsystem.model.dto.DocumentAction.DisplayDocumentActionDto;
+import mk.ukim.finki.routingsystem.security.EmployeePrincipal;
+import mk.ukim.finki.routingsystem.service.DocumentActionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/documents")
+public class DocumentActionRestController {
+
+    private final DocumentActionService documentActionService;
+
+
+    public DocumentActionRestController(DocumentActionService documentActionService) {
+        this.documentActionService = documentActionService;
+    }
+
+    @GetMapping("{documentId}/documentActions")
+    public ResponseEntity<List<DisplayDocumentActionDto>> getAllDocumentActions(@PathVariable Long documentId) {
+
+        return ResponseEntity.ok(documentActionService.findAllForADocument(documentId));
+    }
+
+    @GetMapping("/{documentId}/actionsByMe")
+    public ResponseEntity<List<DisplayDocumentActionDto>> getAllActionsByEmployeeId(@PathVariable Long documentId,
+                                                                                    @AuthenticationPrincipal EmployeePrincipal employeePrincipal) {
+
+        return ResponseEntity.ok(documentActionService.findAllBySpecificEmployee(employeePrincipal.getEmployeeId(), documentId));
+    }
+
+}
